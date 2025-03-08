223ac2f1f1d2073458c789e0e92b5d9eea58ac24
NadaMohamed2243 <106352195+NadaMohamed2243@users.noreply.github.com>
2025-03-08 17:04:22 +0200

    add static post data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
-        return view("posts.index");
+        $posts = [
+            ['id' => 1 , 'title' => 'laravel' , 'posted_by' => 'ahmed' , 'created_at' => '2025-03-08 12:47:00'],
+            ['id' => 2 , 'title' => 'php' , 'posted_by' => 'mohamed' , 'created_at' => '2025-05-10 11:23:00'],
+            ['id' => 3 , 'title' => 'html' , 'posted_by' => 'ahmed' , 'created_at' => '2025-03-08 10:09:00'],
+        ];
+        return view("posts.index",[ 'posts' => $posts ]);
     }
 }
